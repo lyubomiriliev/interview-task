23c518a46e5813d16fbf3e1e9af8d784e9af81d6
Lyubomir Iliev <iliev.lyubomir98@gmail.com>
2025-02-27 15:57:52 +0200

    Add conditional rendering for images and videos

--- components/GamesGrid.tsx
+++ components/GamesGrid.tsx
@@ -29,20 +29,39 @@ const GamesGrid: React.FC<Props> = ({ games }) => {
     <div>
       <h1>Games Grid</h1>
       <ul>
-        {games.slice(0, 10).map((game) => (
-          <li key={game.id}>
-            <p>{game.name}</p>
-            <p>{game.provider}</p>
-            <p>{game.index}</p>
-            <p>{game.category}</p>
-            <Image
-              width={600}
-              height={600}
-              alt={game.name}
-              src={`https://cdn.palmsbet.com${game.imageModern.modern}`}
-            />
-          </li>
-        ))}
+        {games.slice(0, 10).map((game) => {
+          const imageUrl = game.imageModern?.modern;
+          const fileExtension = imageUrl?.split(".").pop()?.toLowerCase();
+
+          return (
+            <li key={game.id}>
+              <p>{game.name}</p>
+              <p>{game.provider}</p>
+              <p>{game.index}</p>
+              <p>{game.category}</p>
+
+              {fileExtension === "webp" ||
+              fileExtension === "jpg" ||
+              fileExtension === "png" ? (
+                <Image
+                  width={600}
+                  height={600}
+                  alt={game.name}
+                  src={`https://cdn.palmsbet.com${game.imageModern.modern}`}
+                />
+              ) : fileExtension === "webm" ? (
+                <video width={600} height={600} autoPlay loop muted playsInline>
+                  <source
+                    src={`https://cdn.palmsbet.com${game.imageModern.modern}`}
+                    type="video/webm"
+                  />
+                </video>
+              ) : (
+                <p>No Image Available</p>
+              )}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
